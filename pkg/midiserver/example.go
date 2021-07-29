788d0594f702adf09c74d47ff5e551b97c954c87
Duncan McGreggor <oubiwann@gmail.com>
2021-07-29 12:49:08 -0500

    Cleaned up example more.

--- pkg/midiserver/example.go
+++ pkg/midiserver/example.go
@@ -1,29 +1,14 @@
 package midiserver
 
 import (
-	"fmt"
-	"io"
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	"gitlab.com/gomidi/midi/reader"
 	"gitlab.com/gomidi/midi/writer"
 	driver "gitlab.com/gomidi/rtmididrv"
 )
 
-type printer struct{}
-
-func (pr printer) noteOn(p *reader.Position, channel, pitch, vel uint8) {
-	fmt.Printf("NoteOn (ch %v: pitch %v vel: %v)", channel, pitch, vel)
-}
-
-func (pr printer) noteOff(p *reader.Position, channel, pitch, vel uint8) {
-	fmt.Printf("NoteOff (ch %v: pitch %v)", channel, pitch)
-}
-
 func Example() {
-
-	var p printer
 
 	drv, err := driver.New()
 	if err != nil {
@@ -63,40 +48,18 @@ func Example() {
 		log.Panic(err)
 	}
 
-	// to disable logging, pass mid.NoLogger() as option
-	rd := reader.New(reader.NoLogger(),
-		// set the callbacks for the messages you are interested in
-		reader.NoteOn(p.noteOn),
-		reader.NoteOff(p.noteOff),
-	)
+	wr := writer.New(out)
+	wr.SetChannel(0) // sets the channel for the next messages
 
-	// to allow reading and writing concurrently in this example
-	// we need a pipe
-	piperd, pipewr := io.Pipe()
+	seconds := 5
+	log.Debugf("Playing note for %d seconds ...", seconds)
 
-	go func() {
-		// wr := writer.New(pipewr)
-		wr := writer.New(out)
-		wr.SetChannel(0) // sets the channel for the next messages
-
-		seconds := 5
-		log.Debugf("Playing note for %d seconds ...", seconds)
-
-		err := writer.NoteOn(wr, 60, 100)
-		if err != nil {
-			log.Panic(err)
-		}
-
-		duration := time.Duration(seconds) * time.Second
-		time.Sleep(duration)
-		writer.NoteOff(wr, 60) // let the note ring for 5 sec
-		pipewr.Close()         // finishes the writing
-	}()
-
-	for {
-		if reader.ReadAllFrom(rd, piperd) == io.EOF {
-			piperd.Close() // finishes the reading
-			break
-		}
+	err = writer.NoteOn(wr, 60, 100)
+	if err != nil {
+		log.Panic(err)
 	}
+
+	duration := time.Duration(seconds) * time.Second
+	time.Sleep(duration)
+	writer.NoteOff(wr, 60) // let the note ring for 5 sec
 }
