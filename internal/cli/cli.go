d2fbc0005f518b7757ca8c6d3b67f6def5106620
Duncan McGreggor <oubiwann@gmail.com>
2021-08-30 21:22:38 -0500

    Updated flags and help text.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -18,13 +18,19 @@ const (
   example [args]
         where the optional positional integer args are device and channel.
         An example piece of music will be played on given device and channel.
+        Valid device numbers are any of the "out" devices in the output of
+        the 'list-devices' command; valid channel numbers are any of the 16
+        MIDI channels: 0 through 15.
   list-devices
-        will list the MIDI devices currently recognised by the operating system,
-       grouped by input devices and output devices.
+        will list the MIDI devices currently recognised by the operating
+        system, grouped by input devices and output devices.
   play-note [args]
-       where the optional positional integer args are device, channel,
-       pitch, velocity, and duration. The pitch will be played with the
-       given argument values for the given duration.
+        where the optional positional integer args are device, channel,
+        pitch, velocity, and duration. The pitch will be played with the
+        given argument values for the given duration. Valid device numbers
+        are any of the "out" devices in the output of the 'list-devices'
+        command; valid channel numbers are any of the 16 MIDI channels:
+        0 through 15.
   ping
         provided for testing purposes by Erlang Ports implementations
   version
@@ -53,22 +59,11 @@ func Parse() *types.Flags {
 	flag.BoolVar(&daemon, "daemon", daemDef, daemUse)
 	flag.BoolVar(&daemon, "d", daemDef, daemUse+shortUse)
 
-	var example bool
-	exampleDef := false
-	exampleUse := "Play some example MIDI; required args are device, " +
-		"channel, pitch, velocity, and duration"
-	flag.BoolVar(&example, "example", exampleDef, exampleUse)
-
 	var loglevel string
 	logDef := "warn"
 	logUse := "Set the logging level"
 	flag.StringVar(&loglevel, "loglevel", logDef, logUse)
 	flag.StringVar(&loglevel, "l", logDef, logUse+shortUse)
-
-	var listing bool
-	listDef := false
-	listUse := "List the system MIDI devices"
-	flag.BoolVar(&listing, "list", listDef, listUse)
 
 	var parser string
 	parsDef := DefaultParser
@@ -76,11 +71,10 @@ func Parse() *types.Flags {
 		ExecParser + ", " +
 		PortParser + ", " +
 		TextParser + "]"
-	parsUse := "Set the parser to user for commands and data. Legal values are " +
-		parsLegals + ". \n" +
-		"Note that setting to 'text' disables daemonisation and setting any of " +
-		"the other \n" +
-		"parsers automatically enables daemonisation"
+	parsUse := "Set the parser to user for commands and data. Legal values are:\n" +
+		parsLegals + ". Note that setting to 'text' disables\n" +
+		"daemonisation and setting any of the other parsers automatically \n" +
+		"enables daemonisation"
 	flag.StringVar(&parser, "parser", parsDef, parsUse)
 	flag.StringVar(&parser, "p", parsDef, parsUse+shortUse)
 
@@ -97,13 +91,12 @@ func Parse() *types.Flags {
 		cmd = types.CommandName(args[0])
 	}
 	flags := &types.Flags{
-		Args:        flag.Args(),
-		Command:     types.Command(cmd),
-		Daemon:      daemon,
-		ListDevices: listing,
-		LogLevel:    loglevel,
-		Parser:      types.Parser(types.ParserName(parser)),
-		Version:     vsn,
+		Args:     flag.Args(),
+		Command:  types.Command(cmd),
+		Daemon:   daemon,
+		LogLevel: loglevel,
+		Parser:   types.Parser(types.ParserName(parser)),
+		Version:  vsn,
 	}
 
 	if flags.Version {
