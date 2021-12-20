a4423d307865ba02f054415951aac5e7bd462192
Duncan McGreggor <oubiwann@gmail.com>
2021-12-19 20:03:39 -0600

    Minor refactor/cleanup.

--- internal/app/setup.go
+++ internal/app/setup.go
@@ -4,11 +4,24 @@ import (
 	"math/rand"
 	"time"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/geomyidia/zylog/logger"
+	"github.com/ut-proj/midiserver/pkg/types"
+	"github.com/ut-proj/midiserver/pkg/version"
 )
 
-// SetupApp ...
-func SetupLogging(logLevel string, reportCaller bool) {
+// Setup
+func Setup(flags *types.Flags) {
+	setupLogging(flags.LogLevel, flags.LogReportCaller)
+	setupRandom()
+	log.Info("Welcome to the Go midiserver!")
+	log.Infof("running version: %s", version.VersionedBuildString())
+	log.Tracef("flags: %+v", flags)
+}
+
+// setupLogging ...
+func setupLogging(logLevel string, reportCaller bool) {
 	logger.SetupLogging(&logger.ZyLogOptions{
 		Colored:      true,
 		Level:        logLevel,
@@ -18,6 +31,7 @@ func SetupLogging(logLevel string, reportCaller bool) {
 
 }
 
-func SetupRandom() {
+// setupRandom ...
+func setupRandom() {
 	rand.Seed(time.Now().Unix())
 }
