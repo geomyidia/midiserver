bf61f0b01c4aeedfc307d7c2f2ce872071edc850
Duncan McGreggor <oubiwann@gmail.com>
2021-12-20 00:26:46 -0600

    Added placholder callbacks for RT messages.

--- pkg/midi/receive.go
+++ pkg/midi/receive.go
@@ -9,3 +9,27 @@ import (
 func ReceiveEach(p *reader.Position, msg midi.Message) {
 	log.Debugf("got MIDI msg %+v (at position %v)", msg, p)
 }
+
+func ReceiveClock() {
+	log.Debug("got clock msg")
+}
+
+func ReceiveContinue() {
+	log.Debug("got continue msg")
+}
+
+func ReceiveReset() {
+	log.Debug("got reset msg")
+}
+
+func ReceiveStart() {
+	log.Debug("got start msg")
+}
+
+func ReceiveStop() {
+	log.Debug("got stop msg")
+}
+
+func ReceiveTick() {
+	log.Debug("got tick msg")
+}
