330af9dbba25b4a072257cf56a52cb01d01973ab
Duncan McGreggor <oubiwann@gmail.com>
2021-12-23 14:29:13 -0600

    Moved epmd stuff into its own subpackage.

--- pkg/commands/remote.go
+++ pkg/commands/remote.go
@@ -5,12 +5,13 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/ut-proj/midiserver/pkg/erl/epmd"
 	"github.com/ut-proj/midiserver/pkg/erl/rpc"
 	"github.com/ut-proj/midiserver/pkg/types"
 )
 
 func ShowRemotePort(flags *types.Flags) {
-	port, err := rpc.NodePort(flags.EPMDHost, flags.EPMDPort, flags.RemoteNode)
+	port, err := epmd.NodePort(flags.EPMDHost, flags.EPMDPort, flags.RemoteNode)
 	if err != nil {
 		log.Error(err)
 	}
--- pkg/erl/rpc/client.go
+++ pkg/erl/rpc/client.go
@@ -1,11 +1,8 @@
 package rpc
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"io"
-	"strings"
 	"time"
 
 	"github.com/ergo-services/ergo"
@@ -14,28 +11,18 @@ import (
 	"github.com/ergo-services/ergo/node"
 	log "github.com/sirupsen/logrus"
 
-	"github.com/ut-proj/midiserver/internal/tcp"
 	"github.com/ut-proj/midiserver/pkg/erl"
 	"github.com/ut-proj/midiserver/pkg/types"
 )
 
 const (
-	PORT_PLEASE2_REQ = 122
-	PORT2_RESP       = 119
-	PORT2_OK         = 0
-	ErrInt           = -1
-	channelBuffers   = 2
-	clockModule      = "undermidi.extclock"
+	channelBuffers = 2
+	clockModule    = "undermidi.extclock"
 )
 
 var (
 	pingTimeout = time.Second * time.Duration(2)
-	// Errors
-	ErrEPMDEmptyReply = errors.New("empty response from epmd")
-	ErrEPMDBadReply   = errors.New("unexpected response from epmd")
-	ErrEPMDNotFound   = errors.New("node not found")
-	ErrBadNodeName    = errors.New("bad node name")
-	ErrTimeout        = errors.New("result timeout")
+	ErrTimeout  = errors.New("result timeout")
 )
 
 type RPC struct {
@@ -149,60 +136,4 @@ func (c *Client) awaitResult() (interface{}, error) {
 	case <-time.After(pingTimeout):
 		return "", ErrTimeout
 	}
-}
-
-// Utility functions
-
-func NodePort(epmdHost string, epmdPort int, remoteNode string) (int, error) {
-	epmdClient, err := tcp.NewClient(epmdHost, epmdPort)
-	if err != nil {
-		return ErrInt, err
-	}
-	defer epmdClient.Close()
-	err = epmdClient.Write(makePortPlease2Req(remoteNode))
-	if err != nil {
-		return ErrInt, err
-	}
-	reply, err := epmdClient.Read()
-	if err != nil && err != io.EOF {
-		return ErrInt, err
-	}
-	if len(reply) == 0 {
-		return ErrInt, ErrEPMDEmptyReply
-	}
-	log.Tracef("raw reply: %v", reply)
-	parsed, err := parseEPMDReply(reply)
-	log.Tracef("parsed data: %v", parsed)
-	if err != nil {
-		return ErrInt, err
-	}
-	return parsed, nil
-}
-
-// Taken from https://github.com/ergo-services/ergo/blob/master/node/epmd.go
-func makePortPlease2Req(remoteNode string) []byte {
-	shortName := strings.Split(remoteNode, erl.NodeDelimit)[0]
-	reqLen := uint16(2 + len(shortName) + 1)
-	req := make([]byte, reqLen)
-	binary.BigEndian.PutUint16(req[0:2], uint16(len(req)-2))
-	req[2] = byte(PORT_PLEASE2_REQ)
-	copy(req[3:reqLen], shortName)
-	return req
-}
-
-func parseEPMDReply(data []byte) (int, error) {
-	distCode := data[0]
-	if distCode != PORT2_RESP {
-		return ErrInt, ErrEPMDBadReply
-	}
-	resultCode := data[1]
-	if resultCode != PORT2_OK {
-		return ErrInt, ErrEPMDNotFound
-	}
-	port := bytesToInt(data[2:4])
-	return int(port), nil
-}
-
-func bytesToInt(b []byte) uint16 {
-	return binary.BigEndian.Uint16(b)
 }
